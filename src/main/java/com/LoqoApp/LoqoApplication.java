5ec7cf5ba5719826ae90a622921971c597763e38
GauravNegi <negigaurav637@gmail.com>
2024-07-20 09:47:46 +0530

    first commit

---
+++ src/main/java/com/LoqoApp/LoqoApplication.java
@@ -0,0 +1,13 @@
+package com.LoqoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoqoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoqoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LoqoApp/LoqoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LoqoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoqoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
